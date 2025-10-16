b4c5cf8c7055bccd542a691e153fe878ac418f9e
James Norris <james.norris.developer@gmail.com>
2025-10-16 08:51:22 -0400

    Initial project setup - Spring Boot backend

---
+++ backend/dash-iq/src/main/java/com/dashiq/DashIqApplication.java
@@ -0,0 +1,13 @@
+package com.dashiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashIqApplication.class, args);
+	}
+
+}
---
+++ backend/dash-iq/src/test/java/com/dashiq/DashIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
